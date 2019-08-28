f28daeafe09e2b86aad7235fbc7756998a97f64c
Mazepa <mfmazepa@hotmail.com>
2019-08-27 21:02:23 -0300

    First commit

--- redis_master.py
+++ redis_master.py
@@ -1,21 +1,32 @@
 from time import sleep
 from redis import Redis
+import numpy
 from rq import Queue
-from redis_modules import print_as_function
+from redis_modules import cria_matriz, multiplica_linha_coluna
 
 if __name__ == "__main__":
-  print "Initializing redis master"
+  linhas, colunas = 3, 3
+
+  matrizA = cria_matriz(linhas, colunas)
+  matrizB = cria_matriz(linhas, colunas)
+  matrizC = numpy.zeros(shape=(linhas, colunas))
+
   redis_conn = Redis(host='127.0.0.1',port=6379)
   queue_jobs = Queue('my_queue', connection=redis_conn)
   jobs = []
-  for i in range(10):
-    job = queue_jobs.enqueue(print_as_function, "realizando trabalho {0}".format(i))
-    jobs.append(job)
 
-  for job in jobs:
-    print "Trabalhos enfileirados {0}".format(len(queue_jobs))
-    while job.result is None:
-      print "O trabalho {0} ainda nao foi concluido".format(job.id)
-      sleep(2)
-      print "Resultado {0}".format(job.result)
-    print job.result
+  for i in range(len(matrizA)):
+    jobLocal = []
+    for j in range(len(matrizA[0])):
+      job = queue_jobs.enqueue(multiplica_linha_coluna, matrizA, matrizB, i, j)
+      jobLocal.append(job)
+    jobs.append(jobLocal)
+
+  for i in range(len(matrizA)):
+    for j in range(len(matrizA[0])):
+      while jobs[i][j].result is None:
+        sleep(2)
+      matrizC[i][j] = jobs[i][j].result
+
+  print matrizC
+
